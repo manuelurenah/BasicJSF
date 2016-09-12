0ca531f1eeccfa9891dc5de0a7b72c8b6389a788
Luis Rojas <lrojas94@gmail.com>
2016-09-12 00:13:08 -0400

    Added table fill in and partial view functionality. Currently, view - SHOULD - be set, but it is not, however, once set, view would work just fine.

--- src/com/cookiebutter/jsf/ManagedBeans/FormContactView.java
+++ src/com/cookiebutter/jsf/ManagedBeans/FormContactView.java
@@ -1,8 +1,14 @@
 package com.cookiebutter.jsf.ManagedBeans;
 
+import com.cookiebutter.jsf.Objects.Contact;
+
+import javax.ejb.SessionContext;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by MEUrena on 9/10/16.
@@ -20,7 +26,25 @@ public class FormContactView implements Serializable {
     private String email;
 
     public String processForm() {
+        Contact contact = new Contact(name, lastname, address, phone, email);
+        FacesContext context = FacesContext.getCurrentInstance();
+        ArrayList<Contact> list = (ArrayList<Contact>)context.getExternalContext().getSessionMap().get("contacts");
+        if(list == null) {
+           list = new ArrayList<Contact>();
+        }
+
+        list.add(contact);
+        context.getExternalContext().getSessionMap().put("contacts", list);
+        clearForm();
         return "index?faces-redirect=true";
+    }
+
+    public void clearForm() {
+        name="";
+        lastname="";
+        address="";
+        phone="";
+        email="";
     }
 
     public String getName() {
---
+++ src/com/cookiebutter/jsf/ManagedBeans/IndexView.java
@@ -0,0 +1,36 @@
+package com.cookiebutter.jsf.ManagedBeans;
+
+import com.cookiebutter.jsf.Objects.Contact;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import java.util.ArrayList;
+
+/**
+ * Created by luis on 11/09/16.
+ */
+@ManagedBean(name = "index")
+@SessionScoped
+public class IndexView {
+
+    private Contact selectedContact;
+
+    public ArrayList<Contact> getContacts() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        return (ArrayList<Contact>)context.getExternalContext().getSessionMap().get("contacts");
+    }
+
+    public void setContacts(ArrayList<Contact> contacts) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getExternalContext().getSessionMap().put("contacts", contacts);
+    }
+
+    public Contact getSelectedContact() {
+        return selectedContact;
+    }
+
+    public void setSelectedContact(Contact selectedContact) {
+        this.selectedContact = selectedContact;
+    }
+}
